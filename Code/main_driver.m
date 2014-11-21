b545389482b1b7263d976c3aba64eb1aeb126b76
Ander Solorzano <robodroid17@gmail.com>
2014-11-21 09:48:47 -0500

    Added Main Code file

---
+++ Code/main_driver.m
@@ -0,0 +1,21 @@
+% MATHEMATICS FUNDAMENTALS FOR ROBOTICS
+%
+% PROJECT: Shadow Traverse Rovers
+% Authors:
+%   Ander Solorzano
+%   Ben Chaffe
+%
+% Date: Fall 2014
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Clear all variables and screen
+%
+clear all variables;
+clc;
+
+
+
+
+
+
